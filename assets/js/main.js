04a1ccd0433d8510bdd9ff932408e8415ba8728f
Ram Kumar <itsramkumar7@gmail.com>
2020-08-07 14:31:41 +0530

    New changes and customization

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,3 +20,19 @@ function setBubble(range, bubble) {
   // Sorta magic numbers based on size of the native UI thumb
   bubble.style.left = `calc(${newVal}% + (${8 - newVal * 0.15}px))`;
 }
+
+window.onload = function(){
+  $("body, html").scrollTop(0);
+  if(this.scrollY > 100) {
+    header[0].classList.add("header-white-bg");
+  }
+};
+
+var header = document.getElementsByTagName("header");
+window.onscroll = function(){
+  if(this.scrollY > 50) {
+    header[0].classList.add("header-white-bg");
+  } else {
+    header[0].classList.remove("header-white-bg");
+  }
+};
